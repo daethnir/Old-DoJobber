74922372d8a1f5e72ff18d152c6d658607407912
Bri Hatch <bri@ifokr.org>
2022-02-21 13:12:30 -0800

    Implement pylint and black changes to dojobber_example.py

--- tests/dojobber_example.py
+++ tests/dojobber_example.py
@@ -19,31 +19,26 @@ import os
 import sys
 
 import dojobber
+
 # These 'from...imports' just decrease typing when making your classes
 from dojobber import Job
 from dojobber import DummyJob
 from dojobber import RunonlyJob
 
-## We like lint, but DoJobber classes get much longer
-## if we implement all of the normal best practices
+# We like lint, but DoJobber classes get much longer
+# if we implement all of the normal best practices
+#
 # pylint:disable=missing-docstring
-# pylint:disable=no-init
-# pylint:disable=no-self-use
-# pylint:disable=too-few-public-methods
-# pylint:disable=unused-argument
 # pylint:disable=invalid-name
-
+# pylint:disable=no-self-use
 
 # This import is used as part of this contrived
 # example - not needed for general DoJobber use.
+# pylint:disable=wrong-import-order
 import random
 
 
-AVAILABLE_MOVIES = [
-    'Noises Off',
-    'MST3K',
-    'Babylon 5'
-]
+AVAILABLE_MOVIES = ['Noises Off', 'MST3K', 'Babylon 5']
 
 
 class CleanCouch(Job):
@@ -52,13 +47,14 @@ class CleanCouch(Job):
     An example of using Job storage - the right way
     to maintain some local state.
     """
+
     DEPS = ()
 
-    def Check(self, *dummy_args, **dummy_kwargs):
+    def Check(self, *_, **__):
         if not self.storage.get('runran'):
             raise ValueError('I fail unless run runs....')
 
-    def Run(self, *dummy_args, **kwargs):
+    def Run(self, *_, **kwargs):
         self.storage['runran'] = True
 
         # This code is just for unit testing purposes, you can ignore it.
@@ -80,12 +76,13 @@ class CleanCouch(Job):
 
 class FindTVRemote(Job):
     """Find the remote."""
+
     DEPS = ()
 
-    def Check(self, *dummy_args, **dummy_kwargs):
+    def Check(self, *_, **__):
         pass
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         pass
 
 
@@ -99,9 +96,10 @@ class FluffPillows(RunonlyJob):
     and will not cause unintended side effects, and we have no way to
     verify that the action is not necessary.
     """
+
     DEPS = ()
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         """If this method fails, then the entire Job fails."""
         logging.info('I am fluffing the pillows in FluffPillows.Run.')
 
@@ -111,14 +109,14 @@ class PickTimeAndDate(Job):
 
     Shows how you can set global state which is used by another Job.
     """
-    TIMES = ('2024-04-08 18:18 UTC',
-             '2099-09-14 16:57 UTC')
 
-    def Check(self, *dummy_args, **dummy_kwargs):
+    TIMES = ('2024-04-08 18:18 UTC', '2099-09-14 16:57 UTC')
+
+    def Check(self, *_, **__):
         # Only successful if we've set our start time
         assert self.global_storage['Start-DateTime']
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         timechoice = random.choice(self.TIMES)
         self.global_storage['Start-DateTime'] = timechoice
 
@@ -130,12 +128,13 @@ class ValidateMovie(Job):
     of the movies we have available on our shelf.
     """
 
-    def Check(self, *dummy_args, **kwargs):
+    def Check(self, *_, **kwargs):
         if kwargs.get('movie') not in AVAILABLE_MOVIES:
-            raise RuntimeError('{} not one of the available movies.'.format(
-                kwargs.get('movie')))
+            raise RuntimeError(
+                f'{kwargs.get("movie")} not one of the available movies.'
+            )
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         pass
 
 
@@ -147,17 +146,19 @@ class InsertDVD(Job):
     In our example this would be closing up the DVD case and putting it back
     on the shelf where you can find it again.
     """
+
     DEPS = (ValidateMovie,)
 
-    def Check(self, *dummy_args, **dummy_kwargs):
+    def Check(self, *_, **__):
         pass
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         pass
 
 
 class PrepareRoom(DummyJob):
     """PrepareRoom is just a DummyJob."""
+
     DEPS = (CleanCouch, FluffPillows)
 
 
@@ -188,6 +189,7 @@ class PopcornBowl(DummyJob):
     between retries of this Job. (Unless your graph is
     highly linear and there are no unblocked Jobs.)
     """
+
     TRIES = 8  # The default is 1, i.e. no retries
     RETRY_DELAY = 0.001
 
@@ -197,9 +199,10 @@ class PopcornBowl(DummyJob):
         if self.global_storage.get('bowl_failcount', 0) < success_try:
             raise RuntimeError("Dishwasher cycle not done yet.")
 
-    def Run(self, *_, **kwargs):
+    def Run(self, *_, **__):
         self.global_storage['bowl_failcount'] = (
-            self.global_storage.get('bowl_failcount', 0) + 1)
+            self.global_storage.get('bowl_failcount', 0) + 1
+        )
 
 
 class Pizza(RunonlyJob):
@@ -208,12 +211,14 @@ class Pizza(RunonlyJob):
     In reality this would make no sense as a RunonlyJob, however
     it is implemented this way here for unit testing purposes.
     """
+
     TRIES = 3
 
     def Run(self, *_, **kwargs):
         # Simulate failures and eventual successes
         self.global_storage['pizza_failcount'] = (
-            self.global_storage.get('pizza_failcount', 0) + 1)
+            self.global_storage.get('pizza_failcount', 0) + 1
+        )
         success_try = kwargs.get('pizza_success_try') or self.TRIES
         if self.global_storage.get('pizza_failcount', 0) < success_try:
             raise RuntimeError("Giordano's did not arrive yet.")
@@ -224,6 +229,7 @@ class Popcorn(Job):
 
     Does retries, similar to Pizza above.
     """
+
     DEPS = (PopcornBowl,)
     # Popcorn.TRIES is intentionally lower than PopcornBowl.TRIES so
     # we assure through unit tests that our retry counting logic is
@@ -236,18 +242,21 @@ class Popcorn(Job):
         if self.global_storage.get('pop_failcount', 0) < success_try:
             raise RuntimeError('Still popping...')
 
-    def Run(self, *_, **kwargs):
+    def Run(self, *_, **__):
         self.global_storage['pop_failcount'] = (
-            self.global_storage.get('pop_failcount', 0) + 1)
+            self.global_storage.get('pop_failcount', 0) + 1
+        )
 
 
 class Food(DummyJob):
     """Get noshies."""
+
     DEPS = (Popcorn, Pizza)
 
 
 class SitOnCouch(Job):
     """Sit on the couch."""
+
     DEPS = (PrepareRoom,)
 
     def Check(self, *_, **kwargs):
@@ -260,6 +269,7 @@ class SitOnCouch(Job):
 
 class DetermineDetails(DummyJob):
     """DeterminDetails is just a DummyJob."""
+
     DEPS = (ValidateMovie, PickTimeAndDate)
 
 
@@ -275,7 +285,8 @@ class InviteFriends(DummyJob):
     work and store the results, and have others depend on it and save
     the repeated work.
     """
-    DEPS = [DetermineDetails,]  # This will be expanded by invite_friends
+
+    DEPS = [DetermineDetails]  # This will be expanded by invite_friends
     INVITE = None
 
 
@@ -289,12 +300,13 @@ class SendInvite(Job):
     You inherit from this and set self.{EMAIL,NAME} and add it to the
     DEPS of the Job where it belongs. See invitation_job for example.
     """
+
     EMAIL = None
     NAME = None
     DEPS = (DetermineDetails,)
     SENT = False
 
-    def Check(self, *_, **dummy_kwargs):
+    def Check(self, *_, **__):
         """Check to see if we sent an email
 
         In real code, you'd need some way to know you
@@ -315,24 +327,24 @@ class SendInvite(Job):
         and we pick up the movie choice and start time from
         global storage.
         """
-        content = 'Come and watch {} at {} with me!'.format(
-            kwargs['movie'],
-            self.global_storage['Start-DateTime'])
-        recipient = '{} <{}>'.format(self.NAME, self.EMAIL)
+        content = (
+            f'Come and watch {kwargs["movie"]} at'
+            f' {self.global_storage["Start-DateTime"]} with me!'
+        )
+        recipient = f'{self.NAME} <{self.EMAIL}>'
 
         # Do something to send the invite. Here's an example
         #  if we were using smtp
         #
-        #msg = MIMEText(content)
-        #msg['Subject'] = 'Come watch a movie with me!'
-        #msg['To'] = recipient
-        #smtp = smtplib.SMTP(host='localhost'
-        #smtp.sendmail(
-        #    os.environ.get('USER') + '@example.com',
-        #    [self.EMAIL]
-        #    msg.as_string())
-        logging.info('Sent {} the following: \n\t{}\n'.format(
-            recipient, content))
+        # msg = MIMEText(content)
+        # msg['Subject'] = 'Come watch a movie with me!'
+        # msg['To'] = recipient
+        # smtp = smtplib.SMTP(host='localhost'
+        # smtp.sendmail(
+        #     os.environ.get('USER') + '@example.com',
+        #     [self.EMAIL]
+        #     msg.as_string())
+        logging.info('Sent %s the following: \n\t%s\n', recipient, content)
         self.SENT = True
 
 
@@ -344,7 +356,7 @@ def invite_friends(people):
     in our list and be executed.
     """
     for person in people:
-        job = type('Invite {}'.format(person['name']), (SendInvite,), {})
+        job = type(f'Invite {person["name"]}', (SendInvite,), {})
         job.EMAIL = person['email']
         job.NAME = person['name']
         InviteFriends.DEPS.append(job)
@@ -353,11 +365,11 @@ def invite_friends(people):
 class FriendsArrive(Job):
     DEPS = (InviteFriends,)
 
-    def Check(self, *dummy_args, **dummy_kwargs):
+    def Check(self, *_, **__):
         # Do something to verify that everyone has arrived.
         pass
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         pass
 
 
@@ -370,13 +382,14 @@ class TurnOnTV(Job):
 
     Unfortunately, they don't. ;-)
     """
+
     DEPS = (FindTVRemote,)
 
-    def Check(self, *dummy_args, **kwargs):
+    def Check(self, *_, **kwargs):
         if kwargs['battery_state'] != 'charged':
             raise RuntimeError('Remote batteries are dead.')
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         # Take out bateries, put them back in
         # Might that fix it?
         # Pretend we did something here....
@@ -386,113 +399,156 @@ class TurnOnTV(Job):
 class StartMovie(Job):
     DEPS = (FriendsArrive, PrepareRoom, TurnOnTV, InsertDVD, SitOnCouch, Food)
 
-    def Check(self, *dummy_args, **dummy_kwargs):
+    def Check(self, *_, **__):
         pass
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         pass
 
 
 class WatchMovie(Job):
     DEPS = (StartMovie,)
 
-    def Check(self, *dummy_args, **dummy_kwargs):
+    def Check(self, *_, **__):
         pass
 
-    def Run(self, *dummy_args, **dummy_kwargs):
+    def Run(self, *_, **__):
         pass
+
 
 def handle_args():
     """Parse our arguments, return args result."""
     myparser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
     myparser.add_argument(
-        '-n', '--no-act', dest='no_act',
+        '-n',
+        '--no-act',
+        dest='no_act',
         action='store_true',
-        help='Do Checks only, make no changes.')
+        help='Do Checks only, make no changes.',
+    )
 
     group = myparser.add_argument_group('Display/Output Options')
     # Verbose mode will output all check/run statuses in a terse mode
-    group.add_argument('-v', dest='verbose', action='store_true',
-                       help='Verbose DoJobber output.')
+    group.add_argument(
+        '-v',
+        dest='verbose',
+        action='store_true',
+        help='Verbose DoJobber output.',
+    )
 
     # Debug will output full stack trace of all check/run failures
-    group.add_argument('-d', dest='debug', action='store_true',
-                       help='Debug DoJobber output.')
+    group.add_argument(
+        '-d', dest='debug', action='store_true', help='Debug DoJobber output.'
+    )
 
     # Debug will output full stack trace of all check/run failures
-    group.add_argument('--app-logs', dest='app_logs', action='store_true',
-        help='Enable example log output. (Not same as DoJobber output, -v/-d)')
+    group.add_argument(
+        '--app-logs',
+        dest='app_logs',
+        action='store_true',
+        help='Enable example log output. (Not same as DoJobber output, -v/-d)',
+    )
 
     # By default we'll display x11 graphs when we think they'll work.
     # Use --no-x11 if you don't have imagemagick installed or
     # your DISPLAY is lying.
     group.add_argument(
-        '--x11', dest='x11', action='store_true',
+        '--x11',
+        dest='x11',
+        action='store_true',
         default=True,
-        help='Try to display result graphs on X11 display')
+        help='Try to display result graphs on X11 display',
+    )
     group.add_argument(
-        '--no-x11', dest='x11', action='store_const',
+        '--no-x11',
+        dest='x11',
+        action='store_const',
         const=False,
-        help='Do not try to display result graphs on X11 display')
+        help='Do not try to display result graphs on X11 display',
+    )
 
     group.add_argument(
-        '--display_prerungraph', dest='display_prerungraph',
+        '--display_prerungraph',
+        dest='display_prerungraph',
         action='store_true',
-        help='Display the Job graph prior to doing anything.')
+        help='Display the Job graph prior to doing anything.',
+    )
 
     # No X11 but you want to see the results image? Save it to
     # a file then.
     group.add_argument(
-        '--png_output', dest='png_output',
-        help='Write the results graph in png form to the given filename')
+        '--png_output',
+        dest='png_output',
+        help='Write the results graph in png form to the given filename',
+    )
 
     group = myparser.add_argument_group(title='Example Input Tweaking')
     # Allow user to pick a movie. We add one that's not available
     # such that you can test a failure via '--movie Zardoz'
     group.add_argument(
-        '--movie', dest='movie',
+        '--movie',
+        dest='movie',
         help='Movie to watch.',
         default=AVAILABLE_MOVIES[0],
-        choices=AVAILABLE_MOVIES + ['Zardoz'])
+        choices=AVAILABLE_MOVIES + ['Zardoz'],
+    )
 
     group.add_argument(
-        '--battery_state', dest='battery_state',
+        '--battery_state',
+        dest='battery_state',
         help='Faux TV Remote Battery State. Try "charged" to make them work.',
-        default='dead')
+        default='dead',
+    )
 
     group.add_argument(
-        '--couch_space', dest='couch_space',
+        '--couch_space',
+        dest='couch_space',
         action='store_true',
-        help='There is room on the couch for you to sit.',)
+        help='There is room on the couch for you to sit.',
+    )
 
     group = myparser.add_argument_group(
         title='Retry Testing',
         description='Try values higher than the max tries to see failures'
-                    ' or smaller to succeed sooner. Best with -v so you can'
-                    ' see how unblocked Jobs are retried interleaved rather'
-                    ' than repeatedly.')
+        ' or smaller to succeed sooner. Best with -v so you can'
+        ' see how unblocked Jobs are retried interleaved rather'
+        ' than repeatedly.',
+    )
     group.add_argument(
-        '--pop_success_try', dest='pop_success', type=int, metavar='#',
+        '--pop_success_try',
+        dest='pop_success',
+        type=int,
+        metavar='#',
         help='Achieve sucess on the Popcorn Job at this number of tries.'
-             ' By default this contrived example will succeed on the last try.'
-             ' Values higher than {} will cause this to fail, so you can test'
-             ' the retry logic.'.format(Popcorn.TRIES))
+        ' By default this contrived example will succeed on the last try.'
+        f' Values higher than {Popcorn.TRIES} will cause this to fail, so'
+        ' you can test the retry logic.',
+    )
 
     group.add_argument(
-        '--bowl_success_try', dest='bowl_success', type=int, metavar='#',
+        '--bowl_success_try',
+        dest='bowl_success',
+        type=int,
+        metavar='#',
         help='Achieve sucess on the PopcornBowl Job at this number of tries.'
-             ' By default this contrived example will succeed on the last try.'
-             ' Values higher than {} will cause this to fail, so you can test'
-             ' the retry logic.'.format(PopcornBowl.TRIES))
+        ' By default this contrived example will succeed on the last try.'
+        f' Values higher than {PopcornBowl.TRIES} will cause this to fail, so'
+        ' you can test the retry logic.',
+    )
 
     group.add_argument(
-        '--pizza_success_try', dest='pizza_success_try', type=int, metavar='#',
+        '--pizza_success_try',
+        dest='pizza_success_try',
+        type=int,
+        metavar='#',
         help='Achieve sucess on the Pizza Job at this number of tries.'
-             ' By default this contrived example will succeed on the last try.'
-             ' Values higher than {} will cause this to fail, so you can test'
-             ' the retry logic.'.format(Pizza.TRIES))
+        ' By default this contrived example will succeed on the last try.'
+        f' Values higher than {Pizza.TRIES} will cause this to fail, so'
+        ' you can test the retry logic.',
+    )
 
     # Parsing time!
     args = myparser.parse_args()
@@ -502,7 +558,7 @@ def handle_args():
         logging.basicConfig(level=logging.INFO)
 
     # Determine if we should show graphs
-    args.x11 = True if os.environ.get('DISPLAY') and args.x11 else False
+    args.x11 = bool(os.environ.get('DISPLAY') and args.x11)
 
     return args
 
@@ -512,10 +568,12 @@ def main():
 
     # Add individual friend invite Jobs to our InviteFriends Job list.
     # Pretend these came from command line args, or a database, or whatever.
-    invite_friends([
-        {'name': 'Wendell Bagg', 'email': 'bagg@example.com'},
-        {'name': 'Lawyer Cat', 'email': 'lawyercat@example.com'},
-    ])
+    invite_friends(
+        [
+            {'name': 'Wendell Bagg', 'email': 'bagg@example.com'},
+            {'name': 'Lawyer Cat', 'email': 'lawyercat@example.com'},
+        ]
+    )
 
     dojob = dojobber.DoJobber()
     dojob.configure(
@@ -523,18 +581,19 @@ def main():
         no_act=args.no_act,
         verbose=args.verbose,
         default_retry_delay=0,
-        debug=args.debug)
+        debug=args.debug,
+    )
 
-    ## Since all our argument names are the same as the kwargs keys,
-    ## we can simply send in the args dictionary as-is.
+    # Since all our argument names are the same as the kwargs keys,
+    # we can simply send in the args dictionary as-is.
     dojob.set_args(**args.__dict__)
 
-    ## If you wanted to do it manually, then you could do it similar to this:
+    # If you wanted to do it manually, then you could do it similar to this:
     #
-    # dojob.set_args(
-    #    movie=args.movie,
-    #    battery_state=args.battery_state,
-    #    couch_space=args.couch_space)
+    #   dojob.set_args(
+    #        movie=args.movie,
+    #        battery_state=args.battery_state,
+    #       couch_space=args.couch_space)
 
     if args.x11 and args.display_prerungraph:
         dojob.display_graph()
@@ -546,8 +605,8 @@ def main():
         dojob.display_graph()
 
     if args.png_output:
-        out = open(args.png_output, 'w')
-        dojob.write_graph(out)
+        with open(args.png_output, 'w', encoding='utf8') as out:
+            dojob.write_graph(out)
 
     sys.exit(0 if dojob.success() else 1)
 
